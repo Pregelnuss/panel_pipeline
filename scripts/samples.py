7d048a174e8b226d52c6cea9bd3fe603046cf4eb
ninao99 <ninao99@.fu-berlin.de>
2022-05-06 05:56:39 +0200

    update samples.py, update comments, folder structure correct now

--- scripts/samples.py
+++ scripts/samples.py
@@ -2,14 +2,20 @@ import pandas as pd
 from pathlib import Path
 import os
 
+# assuming the fastq files are one folder up from scripts and then down in data
+# maybe it's the easiest if the fastq files are symlinked, so no copying is needed
+
 data_dir = Path.cwd() / "../data/"
 #print(os.listdir(data_dir))
 
+# get all files with right ending
 pathlist = data_dir.glob('*.fastq.gz')
 
+# create new dictionary
 dic = {}
-# old samples look like: ERR024609_tiny_2.fastq.gz
+
 # samples on cluster look like: AS-529648-LR-53242_R1.fastq.gz
+# if they look different, the splitting below must be changed
 for path in pathlist:
     path_str = str(path)
     sample = os.path.basename(path).split('_')[0]
@@ -23,5 +29,6 @@ for path in pathlist:
 df = pd.DataFrame.from_dict(dic, orient='index')
 #print(df)
 
+# write file which is then in the workflow being used for mapping
 df.to_csv('sample.tsv', sep="\t", index=False, header= ["samples", "fq1", "fq2"])
     
